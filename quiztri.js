a68f8d9c85bf694776dc3c8a10caa0103c511faf
Prathmesh <pratmbr@gmail.com>
2022-11-01 23:43:00 +0530

    style file updated

--- quiztri.js
+++ quiztri.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEle = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = ["90°", "right angled", "one right angle", "12, 16, 20", "Equilateral Triangle", "85°", "30°", "a + b + c", "no", "45°" ];
 
 function calculateScore(){
     let score = 0;
